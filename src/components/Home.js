793684ca19a6f1a39646a3eae88a4f98adc400d4
Munawar Hussain <munawarhussa.7@gmail.com>
2023-08-12 15:32:05 +0500

    Reveal effect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import {motion } from 'framer-motion'
 function Home() {
     return (
         <div style={{height:'100vh', display:'flex', flexDirection:'column', justifyContent:'center'}}>
-            <h1>Home</h1>
+            <motion.div initial={{opacity:0} } animate={{opacity:1}} transition={{delay:0.5, duration:3}}>
+            <h1>Welcome </h1>   
+            <p>To the world of animation</p>
+            </motion.div>
         </div>
     )
 }
