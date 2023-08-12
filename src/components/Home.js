fd16e225d2f25d0c242dbfc936d4426fcf844ea5
Munawar Hussain <munawarhussa.7@gmail.com>
2023-08-12 15:34:32 +0500

    Added type and stifness

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ import { motion } from 'framer-motion'
 function Home() {
     return (
         <div style={{ height: "100vh", display:"flex", flexDirection:'column' ,alignItems:"center", justifyContent:"center" }}>
-            <motion.div initial={{y:-250}} animate={{y:10}}>
+            <motion.div initial={{y:-250}} animate={{y:10}} transition={{delay:0.1, type:"spring", stiffness:120}}> 
+            {/* stiffness means how much bouncy it should look */}
                 <h1>Welcome to Home </h1>
             </motion.div>
             <motion.div initial={{x:'-100vw'}} animate={{x:0}}>
