71e8ef4e8a0c2c1b6f9e538b1eee93f9b8b1240f
Munawar Hussain <munawarhussa.7@gmail.com>
2023-08-12 13:15:27 +0500

    Clean code

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div style={{height:'100vh', display:'flex', flexDirection:'column', justifyContent:'center'}}>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home
